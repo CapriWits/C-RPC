74fc9dcc92cda1914ffca069c536d1fdf7692142
hypocrite30 <1027093394@qq.com>
2021-12-05 21:56:41 +0800

    feat: update rpc api

--- rpc-service-api/src/main/java/me/hypocrite30/rpc/api/EchoService.java
+++ rpc-service-api/src/main/java/me/hypocrite30/rpc/api/EchoService.java
@@ -1,10 +1,11 @@
 package me.hypocrite30.rpc.api;
 
 /**
- * @Description: 服务端提供的服务 api
+ * Service provider interface api
+ *
  * @Author: Hypocrite30
  * @Date: 2021/11/16 21:48
  */
 public interface EchoService {
-    String testMethod(Entity entity);
+    String echo(Entity entity);
 }
--- rpc-service-api/src/main/java/me/hypocrite30/rpc/api/Entity.java
+++ rpc-service-api/src/main/java/me/hypocrite30/rpc/api/Entity.java
@@ -1,6 +1,7 @@
 package me.hypocrite30.rpc.api;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Entity implements Serializable {
     private String message;
     private String description;
